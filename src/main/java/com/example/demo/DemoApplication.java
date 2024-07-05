2718e9ba642f6f527baf7f4d0c7fe316a5667bdd
sureshn82-coder <sureshnit82@gmail.com>
2024-07-05 19:24:50 +0530

    Update DemoApplication.java
    change
    
    Signed-off-by: sureshn82-coder <sureshnit82@gmail.com>

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+import org.springframework.ai.embedding.EmbeddingClient;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +15,12 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+}
+
+@Configuration
+class AppConfig {
+	@Bean
+	VectorStore vectorStore(EmbeddingClient embedding) {
+		return new SimpleVectorStore(embedding);
+	}
 }
